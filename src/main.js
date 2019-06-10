ee8acfa8efb66c4c251b97477306d14e11a8c616
didagu <didagou@gmail.com>
2019-06-10 19:02:36 +0100

    Include bootstrap-vue and vuex router sync in main.js

--- src/main.js
+++ src/main.js
@@ -2,11 +2,23 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import BootstrapVue from "bootstrap-vue";
+import { sync } from "vuex-router-sync";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
+
+// eslint-disable-next-line no-unused-vars
+const unsync = sync(store, router);
 
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount("#app");
+
+// unsync() /***Used to unsync the store from the router after tearind down the Vue instance in order to release Vue resources*/
