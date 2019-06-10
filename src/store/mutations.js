68b8c490a96bcdf6c6b672251d81124848b371c8
didagu <didagou@gmail.com>
2019-06-11 00:51:15 +0100

    Add login mutations

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -10,5 +10,29 @@ export default {
   },
   SET_ROWS: (state, payload) => {
     state.rows = payload;
+  },
+  LOGIN_REQUEST(state, user) {
+    state.status = {
+      loggingIn: true
+    };
+    state.user = user;
+  },
+  LOGIN_SUCCESS(state, user) {
+    state.status = {
+      loggedIn: true
+    };
+    state.user = user;
+  },
+  LOGIN_FAILURE(state) {
+    state.status = {
+      loggedIn: false
+    };
+    state.user = null;
+  },
+  LOGOUT(state) {
+    state.status = {
+      loggedIn: false
+    };
+    state.user = null;
   }
 };
