a21fe09518217f0626d6780be1c71b835b464bad
didagu <didagou@gmail.com>
2019-06-11 00:52:08 +0100

    Add login and logout button to Header vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,27 @@
 <template>
     <div>
-        <b-nav tabs>
-            <b-nav-item href="/"><b>Random Users App</b></b-nav-item>
-        </b-nav>
+        <b-navbar toggleable="lg" type="dark" variant="primary">
+            <b-navbar-brand href="/">Random Users App</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto">
+                    <b-button v-if="!status.loggedIn" size="sm" class="my-2 my-sm-0" type="submit" variant="primary" href="/login">Login</b-button>
+                    <b-button v-if="status.loggedIn" size="sm" class="my-2 my-sm-0" type="submit" variant="secondary" href="/login">Logout</b-button>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>       
     </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
-    name: "Header"
+    name: "Header",
+    computed: {
+        ...mapState(["status"])
+    }    
 }
 </script>
