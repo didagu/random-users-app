89720c42f780453b6b361f4eeea700a89d14f3ca
didagu <didagou@gmail.com>
2019-06-11 00:50:34 +0100

    Add user logged in status to vuex state

--- src/store/state.js
+++ src/store/state.js
@@ -1,6 +1,11 @@
+const user = JSON.parse(localStorage.getItem("user"));
+let status = user ? { loggedIn: true } : { loggedIn: false };
+
 export default {
   usersCompleteInfo: [],
   usersBasicInfo: [],
   usersAdvancedInfo: [],
-  rows: 0
+  rows: 0,
+  status,
+  user
 };
