3c5aa6baba984e58fb578d5d25cb70c7eaec1866
didagu <didagou@gmail.com>
2019-06-10 18:53:29 +0100

    Create Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <b-nav tabs>
+            <b-nav-item href="/"><b>Random Users App</b></b-nav-item>
+        </b-nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
