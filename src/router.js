72a27611e9c72f3c31be49ba86debdc09f717971
didagu <didagou@gmail.com>
2019-06-10 19:03:23 +0100

    Remove about route and Add routes for users and user

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import User from "./views/User.vue";
+import Users from "./views/Users.vue";
 
 Vue.use(Router);
 
@@ -11,16 +12,17 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      component: Home
+      redirect: "/users"
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/users",
+      name: "users",
+      component: Users
+    },
+    {
+      path: "/users/:userId",
+      name: "user",
+      component: User
     }
   ]
 });
