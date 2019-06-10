adb705a0102544ae1beaff0183f6859d3ad53051
didagu <didagou@gmail.com>
2019-06-11 00:48:28 +0100

    Add users whitelist for login

--- src/services/api/users.js
+++ src/services/api/users.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import users_whitelist from "@/services/data/users_whitelist";
 
 export default {
   getUsers() {
@@ -9,5 +10,27 @@ export default {
         return res.data.results;
       })
       .catch(err => console.log(err));
+  },
+  login(username, password) {
+    const exist = users_whitelist.some(user => {
+      return user.username === username && user.password === password;
+    });
+    return new Promise((resolve, reject) => {
+      if (exist) {
+        localStorage.setItem(
+          "user",
+          JSON.stringify({
+            username,
+            password
+          })
+        );
+        resolve(username);
+      } else {
+        reject(new Error("Invalid credentials, user does not exist"));
+      }
+    });
+  },
+  logout() {
+    localStorage.removeItem("user");
   }
 };
---
+++ src/services/data/users_whitelist.js
@@ -0,0 +1,16 @@
+const users_whitelist = [
+  {
+    username: "admin",
+    password: "admin"
+  },
+  {
+    username: "abas-erp",
+    password: "abas-erp"
+  },
+  {
+    username: "dominic",
+    password: "dominic"
+  }
+];
+
+export default users_whitelist;
