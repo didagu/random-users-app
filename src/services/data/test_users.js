06edba7f6df036ea2846db7c01e81b7e4ee5779a
didagu <didagou@gmail.com>
2019-06-12 13:43:45 +0100

    Add test users array for test purpose

---
+++ src/services/data/test_users.js
@@ -0,0 +1,214 @@
+const test_users = [
+  {
+    id: 0,
+    name: "esat avan",
+    gender: "male",
+    age: 64,
+    address: "6286 bağdat cd, 62680 adıyaman"
+  },
+  {
+    id: 1,
+    name: "sofia serrano",
+    gender: "female",
+    age: 28,
+    address: "9010 avenida de castilla, 69984 san sebastián"
+  },
+  {
+    id: 2,
+    name: "mark lopez",
+    gender: "male",
+    age: 54,
+    address: "940 park road, AK5 9SG preston"
+  },
+  {
+    id: 3,
+    name: "alejandro david",
+    gender: "male",
+    age: 60,
+    address: "9569 avenue joliot curie, 9858 oberrohrdorf"
+  },
+  {
+    id: 4,
+    name: "رهام کوتی",
+    gender: "male",
+    age: 39,
+    address: "5513 حقانی, 53857 قم"
+  },
+  {
+    id: 5,
+    name: "niels brede",
+    gender: "male",
+    age: 56,
+    address: "danziger straße 138, 62417 bad waldsee"
+  },
+  {
+    id: 6,
+    name: "ben pearson",
+    gender: "male",
+    age: 43,
+    address: "5367 station road, RD77 4PN plymouth"
+  },
+  {
+    id: 7,
+    name: "bojan masson",
+    gender: "male",
+    age: 37,
+    address: "2466 rue de cuire, 7279 kemmental"
+  },
+  {
+    id: 8,
+    name: "janik leroux",
+    gender: "male",
+    age: 47,
+    address: "8945 rue paul-duvivier, 4280 wichtrach"
+  },
+  {
+    id: 9,
+    name: "tilia fevang",
+    gender: "female",
+    age: 27,
+    address: "priorveien 3557, 0593 brandal"
+  },
+  {
+    id: 10,
+    name: "batur erberk",
+    gender: "male",
+    age: 57,
+    address: "4769 atatürk sk, 39933 siirt"
+  },
+  {
+    id: 11,
+    name: "neiva rodrigues",
+    gender: "female",
+    age: 41,
+    address: "2628 rua quatro, 89420 macapá"
+  },
+  {
+    id: 12,
+    name: "jeanne ma",
+    gender: "female",
+    age: 69,
+    address: "5693 3rd st, T4K 9Y3 clinton"
+  },
+  {
+    id: 13,
+    name: "saana korhonen",
+    gender: "female",
+    age: 51,
+    address: "5454 pyynikintie, 64316 puolanka"
+  },
+  {
+    id: 14,
+    name: "hans-günther stangl",
+    gender: "male",
+    age: 67,
+    address: "gartenweg 81, 17956 kindelbrück"
+  },
+  {
+    id: 15,
+    name: "rieke elsinga",
+    gender: "female",
+    age: 52,
+    address: "3550 hanengeschrei, 45129 enkhuizen"
+  },
+  {
+    id: 16,
+    name: "viola da silva",
+    gender: "female",
+    age: 27,
+    address: "1952 rue des ecrivains, 3047 bonstetten"
+  },
+  {
+    id: 17,
+    name: "terry berry",
+    gender: "female",
+    age: 28,
+    address: "5217 w campbell ave, 4636 perth"
+  },
+  {
+    id: 18,
+    name: "murielle mercier",
+    gender: "female",
+    age: 67,
+    address: "9178 cours charlemagne, 9581 court"
+  },
+  {
+    id: 19,
+    name: "مهراد علیزاده",
+    gender: "male",
+    age: 25,
+    address: "798 میدان 7 تیر, 18739 ساری"
+  },
+  {
+    id: 20,
+    name: "سام گلشن",
+    gender: "male",
+    age: 38,
+    address: "7433 میرداماد, 61350 نیشابور"
+  },
+  {
+    id: 21,
+    name: "marie olson",
+    gender: "female",
+    age: 70,
+    address: "6846 new road, 57020 shannon"
+  },
+  {
+    id: 22,
+    name: "ragna tolo",
+    gender: "female",
+    age: 70,
+    address: "røahagan 507, 6160 kjellmyra"
+  },
+  {
+    id: 23,
+    name: "geertruida van reeuwijk",
+    gender: "female",
+    age: 25,
+    address: "3283 vondellaan, 17643 stadskanaal"
+  },
+  {
+    id: 24,
+    name: "harper green",
+    gender: "female",
+    age: 53,
+    address: "9926 worcester street, 99060 auckland"
+  },
+  {
+    id: 25,
+    name: "friederike baumgärtner",
+    gender: "female",
+    age: 55,
+    address: "uhlandstraße 141, 47933 lohmar"
+  },
+  {
+    id: 26,
+    name: "اميرحسين مرادی",
+    gender: "male",
+    age: 46,
+    address: "6784 شهید کبیری طامه, 33880 رشت"
+  },
+  {
+    id: 27,
+    name: "مرسانا یاسمی",
+    gender: "female",
+    age: 40,
+    address: "9193 میدان شهید نامجو, 74616 قم"
+  },
+  {
+    id: 28,
+    name: "johan christensen",
+    gender: "male",
+    age: 34,
+    address: "3090 skovkrogen, 40100 nr åby"
+  },
+  {
+    id: 29,
+    name: "murat babacan",
+    gender: "male",
+    age: 30,
+    address: "7485 anafartalar cd, 77080 amasya"
+  }
+];
+
+export default test_users;
